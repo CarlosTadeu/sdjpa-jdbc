4eb1f6ba96b3709e3187f6eec23c84d14f83ffe4
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-01-07 13:54:23 -0300

    Initial commit

---
+++ src/main/java/com/carlostadeu/sdjpajdbc/domain/Author.java
@@ -0,0 +1,65 @@
+package com.carlostadeu.sdjpajdbc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author book = (Author) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String title) {
+        this.firstName = title;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String isbn) {
+        this.lastName = isbn;
+    }
+}
---
+++ src/test/java/com/carlostadeu/sdjpajdbc/MySQLIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.carlostadeu.sdjpajdbc;
+
+import com.carlostadeu.sdjpajdbc.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySQLIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySql() {
+        long countBefore = bookRepository.count();
+        Assertions.assertThat(countBefore).isGreaterThan(0);
+    }
+}
