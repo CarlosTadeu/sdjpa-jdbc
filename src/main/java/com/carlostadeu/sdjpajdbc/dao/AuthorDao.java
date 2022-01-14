238654145a49f40dcd1b6c5c606c557322096f09
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-01-14 14:43:42 -0300

    implement get author by id

---
+++ src/main/java/com/carlostadeu/sdjpajdbc/dao/AuthorDao.java
@@ -0,0 +1,8 @@
+package com.carlostadeu.sdjpajdbc.dao;
+
+import com.carlostadeu.sdjpajdbc.domain.Author;
+
+public interface AuthorDao {
+
+    Author getById(Long id);
+}
---
+++ src/test/java/com/carlostadeu/sdjpajdbc/AuthorDaoIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.carlostadeu.sdjpajdbc;
+
+import com.carlostadeu.sdjpajdbc.dao.AuthorDao;
+import com.carlostadeu.sdjpajdbc.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"com.carlostadeu.sdjpajdbc.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AuthorDaoIntegrationTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Test
+    void testGetAuthor() {
+        Author author = authorDao.getById(1L);
+        assertThat(author).isNotNull();
+    }
+}
